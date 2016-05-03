d4a0577b5b722a1e2bb331f153b347c18fccff8c
vinacovre <viniassis@me.com>
2016-05-03 00:20:19 -0400

    interaction with db OK

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,20 @@
 class TaskController < ApplicationController
+  def index
+    id = params[:list_id]
+    @is_deleted = params[:is_deleted]
+
+    @list = List.find(id)
+
+    @rs_tasks = Task.where("list_id = ?",id)
+
+    if @rs_tasks
+      @tasks = @rs_tasks.where("is_deleted = #{@is_deleted}")
+    else
+      @tasks = nil
+    end
+
+    respond_to do |format|
+      format.js { render :found_tasks }
+    end
+  end
 end
